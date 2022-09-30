f4a2529de7b73f5c7939907840296d89f0a3e474
Chidubem Ani <ifeatu96@gmail.com>
2022-09-30 11:13:21 -0700

    lessons 1 and 2

--- course-04/exercises/lesson-1/starter-code/index.js
+++ course-04/exercises/lesson-1/starter-code/index.js
@@ -1,44 +1,65 @@
-const AWS = require('aws-sdk')
-const axios = require('axios')
+const AWS = require("aws-sdk");
+const axios = require("axios");
 
 // Name of a service, any string
-const serviceName = process.env.SERVICE_NAME
+const serviceName = process.env.SERVICE_NAME;
 // URL of a service to test
-const url = process.env.URL
+const url = process.env.URL;
 
 // CloudWatch client
 const cloudwatch = new AWS.CloudWatch();
 
-exports.handler = async (event) => {
-  // TODO: Use these variables to record metric values
-  let endTime
-  let requestWasSuccessful
+exports.handler = async event => {
+  let endTime;
+  let requestWasSuccessful = 1;
 
-  const startTime = timeInMs()
-  await axios.get(url)
+  const startTime = timeInMs();
+  try {
+    await axios.get(url);
+    endTime = timeInMs();
+  } catch (error) {
+    requestWasSuccessful = 0;
+  }
 
-  // Example of how to write a single data point
-  // await cloudwatch.putMetricData({
-  //   MetricData: [
-  //     {
-  //       MetricName: 'MetricName', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
-  //       Dimensions: [
-  //         {
-  //           Name: 'ServiceName',
-  //           Value: serviceName
-  //         }
-  //       ],
-  //       Unit: '', // 'Count' or 'Milliseconds'
-  //       Value: 0 // Total value
-  //     }
-  //   ],
-  //   Namespace: 'Udacity/Serveless'
-  // }).promise()
+  await cloudwatch
+    .putMetricData({
+      MetricData: [
+        {
+          MetricName: "Latency",
+          Dimensions: [
+            {
+              Name: "ServiceName",
+              Value: serviceName
+            }
+          ],
+          Unit: "Milliseconds",
+          Value: endTime - startTime
+        }
+      ],
+      Namespace: "Udacity/Serveless"
+    })
+    .promise();
 
-  // TODO: Record time it took to get a response
-  // TODO: Record if a response was successful or not
-}
+  await cloudwatch
+    .putMetricData({
+      MetricData: [
+        {
+          MetricName: "Successful",
+          Dimensions: [
+            {
+              Name: "ServiceName",
+              Value: serviceName
+            }
+          ],
+          Unit: "Count",
+          Value: requestWasSuccessful
+        }
+      ],
+      Namespace: "Udacity/Serveless"
+    })
+    .promise();
+};
 
 function timeInMs() {
-  return new Date().getTime()
+  return new Date().getTime();
 }
--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,2 @@
-export const apiEndpoint = '...'
+export const apiEndpoint =
+  "https://u6ehptsn93.execute-api.us-east-1.amazonaws.com/dev";
