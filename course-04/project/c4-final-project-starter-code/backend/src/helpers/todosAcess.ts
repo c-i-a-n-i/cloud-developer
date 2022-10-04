e6fb138b7daae6bf33292214f9a6925d0e11c459
Chidubem Ani <ifeatu96@gmail.com>
2022-10-04 16:52:35 -0700

    submission 1

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -3,10 +3,137 @@ import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate';
+import { TodoUpdate } from '../models/TodoUpdate'
+import { String } from 'aws-sdk/clients/batch'
 
 const XAWS = AWSXRay.captureAWS(AWS)
-
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
 const logger = createLogger('TodosAccess')
 
-// TODO: Implement the dataLayer logic
+export class TodosAccess {
+  constructor(
+    private readonly docClient: DocumentClient = createDynamoDBClient(),
+    private readonly todosTable = process.env.TODOS_TABLE,
+    private readonly bucketName = process.env.ATTACHMENTS_S3_BUCKET,
+    private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
+  ) {}
+
+  async getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.log('1', `Getting todos for user ${userId}`)
+    const result = await this.docClient
+      .query({
+        TableName: this.todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+          ':userId': userId
+        },
+        ScanIndexForward: false
+      })
+      .promise()
+
+    return result.Items as TodoItem[]
+  }
+
+  async createTodo(newItem: TodoItem): Promise<TodoItem> {
+    logger.log(
+      '1',
+      `Getting todo item ${newItem.todoId} for user ${newItem.userId}`
+    )
+    await this.docClient
+      .put({
+        TableName: this.todosTable,
+        Item: newItem
+      })
+      .promise()
+
+    return newItem
+  }
+
+  async updateTodo(
+    userId: string,
+    todoId: string,
+    updatedTodo: TodoUpdate
+  ): Promise<void> {
+    logger.log('1', `Updating todo item ${todoId} for user ${userId}`)
+
+    await this.docClient
+      .update({
+        TableName: this.todosTable,
+        Key: {
+          userId,
+          todoId
+        },
+        UpdateExpression: 'set #name = :name, dueDate = :dueDate, done = :done',
+        ExpressionAttributeValues: {
+          ':name': updatedTodo.name,
+          ':dueDate': updatedTodo.dueDate,
+          ':done': updatedTodo.done
+        },
+        ExpressionAttributeNames: {
+          '#name': 'name'
+        }
+      })
+      .promise()
+  }
+
+  async deleteTodo(userId: string, todoId: string): Promise<void> {
+    logger.log('1', `Deleting todo item ${todoId} for user ${userId}`)
+    await this.docClient
+      .delete({
+        TableName: this.todosTable,
+        Key: {
+          userId,
+          todoId
+        }
+      })
+      .promise()
+  }
+
+  async uploadAttachment(
+    userId: string,
+    todoId: string,
+    imageId: String
+  ): Promise<string> {
+    logger.log(
+      '1',
+      `Uploading attachment to todo item ${todoId} for user ${userId}`
+    )
+    await this.updateAttachmentUrl(userId, todoId, imageId)
+
+    return s3.getSignedUrl('putObject', {
+      Bucket: this.bucketName,
+      Key: imageId,
+      Expires: this.urlExpiration
+    })
+  }
+
+  async updateAttachmentUrl(userId: string, todoId: string, imageId: string) {
+    return await this.docClient
+      .update({
+        TableName: this.todosTable,
+        Key: {
+          userId,
+          todoId
+        },
+        UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+        ExpressionAttributeValues: {
+          ':attachmentUrl': `https://${this.bucketName}.s3.amazonaws.com/${imageId}`
+        }
+      })
+      .promise()
+  }
+}
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('Creating a local DynamoDB instance')
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  }
+
+  return new XAWS.DynamoDB.DocumentClient()
+}
